a72413baeed70fa0467a0a4d4b59a424256efe36
itsu1140 <n.itsuki1140@gmail.com>
2024-12-10 20:40:28 +0900

    ruff format & if refuctoring

--- src/twi.py
+++ src/twi.py
@@ -11,74 +11,91 @@ twi_num = 30
 
 
 async def main(cookie_path):
-
     client = Client(language="ja")
     try:
         if os.path.exists(cookie_path):
             client.load_cookies(cookie_path)
         else:
             raise FileNotFoundError(
-                errno.ENOENT, os.strerror(errno.ENOENT), cookie_path)
+                errno.ENOENT, os.strerror(errno.ENOENT), cookie_path
+            )
 
         bookmarks = await client.get_bookmarks(count=twi_num)
 
         items = []
         for bookmark in bookmarks:
-            if not hasattr(bookmark, 'media') or not bookmark.media:
+            item = {
+                "tags": [],
+                "modificationTime": int(datetime.now(timezone.utc).timestamp() * 1000),
+                "headers": {"referer": "x.com"},
+            }
+            if not hasattr(bookmark, "media") or not bookmark.media:
                 if bookmark.thumbnail_url:
-                    items.append({
-                        "url": bookmark.thumbnail_url,
-                        "name": f"@{bookmark.user.screen_name} {bookmark.text}",
-                        "website": f"https://x.com/{bookmark.user.screen_name}/status/{bookmark.id}/",
-                        "tags": [],
-                        "modificationTime": int(datetime.now(timezone.utc).timestamp()*1000),
-                        "headers": {
-                            "referer": "x.com"
+                    items.append(
+                        {
+                            "url": bookmark.thumbnail_url,
+                            "name": f"@{bookmark.user.screen_name} {bookmark.text}",
+                            "website": f"https://x.com/{bookmark.user.screen_name}/status/{bookmark.id}/",
+                            "tags": [],
+                            "modificationTime": int(
+                                datetime.now(timezone.utc).timestamp() * 1000
+                            ),
+                            "headers": {"referer": "x.com"},
                         }
-                    })
-                if hasattr(bookmark, 'quote') and bookmark.quote is not None:
-                    if hasattr(bookmark.quote, 'media') and bookmark.quote.media is not None:
-                        items.append({
-                            "url": bookmark.quote.media[0].get('media_url_https'),
+                    )
+                if (
+                    hasattr(bookmark, "quote")
+                    and bookmark.quote is not None
+                    and hasattr(bookmark.quote, "media")
+                    and bookmark.quote.media is not None
+                ):
+                    items.append(
+                        {
+                            "url": bookmark.quote.media[0].get("media_url_https"),
                             "name": f"@{bookmark.quote.user.screen_name} {bookmark.quote.text}",
                             "website": f"https://x.com/{bookmark.quote.user.screen_name}/status/{bookmark.quote.id}/",
                             "tags": [],
-                            "modificationTime": int(datetime.now(timezone.utc).timestamp()*1000),
-                            "headers": {
-                                "referer": "x.com"
-                            }
-                        })
-                    continue
+                            "modificationTime": int(
+                                datetime.now(timezone.utc).timestamp() * 1000
+                            ),
+                            "headers": {"referer": "x.com"},
+                        }
+                    )
                 continue
-            for i, media in enumerate(bookmark.media):
-                items.append({
-                    "url": media.get('media_url_https'),
-                    "name": f"@{bookmark.user.screen_name} {bookmark.text}",
-                    "website": f"https://x.com/{bookmark.user.screen_name}/status/{bookmark.id}/",
-                    "tags": [],
-                    "modificationTime": int(datetime.now(timezone.utc).timestamp()*1000),
-                    "headers": {
-                        "referer": "x.com"
+            for media in bookmark.media:
+                items.append(
+                    {
+                        "url": media.get("media_url_https"),
+                        "name": f"@{bookmark.user.screen_name} {bookmark.text}",
+                        "website": f"https://x.com/{bookmark.user.screen_name}/status/{bookmark.id}/",
+                        "tags": [],
+                        "modificationTime": int(
+                            datetime.now(timezone.utc).timestamp() * 1000
+                        ),
+                        "headers": {"referer": "x.com"},
                     }
-                })
+                )
 
-        data = {
-            "items": items,
-        }
+        data = {"items": items}
 
         # Send the POST request
         async with aiohttp.ClientSession() as session:
-            async with session.post(f"http://127.0.0.1:41595/api/item/addFromURLs", json=data) as response:
+            async with session.post(
+                "http://127.0.0.1:41595/api/item/addFromURLs", json=data
+            ) as response:
                 if response.status == 200:
                     result = await response.json()
                     print(result)
                     for bookmark in bookmarks:
-                        if not hasattr(bookmark, 'media') or not bookmark.media:
-                            if bookmark.thumbnail_url:
+                        if not hasattr(bookmark, "media") or not bookmark.media:
+                            if (
+                                bookmark.thumbnail_url
+                                or hasattr(bookmark, "quote")
+                                and bookmark.quote is not None
+                                and hasattr(bookmark.quote, "media")
+                                and bookmark.quote.media is not None
+                            ):
                                 await bookmark.delete_bookmark()
-                            if hasattr(bookmark, 'quote') and bookmark.quote is not None:
-                                if hasattr(bookmark.quote, 'media') and bookmark.quote.media is not None:
-                                    await bookmark.delete_bookmark()
                             continue
                         await bookmark.delete_bookmark()
 
@@ -86,11 +103,13 @@ async def main(cookie_path):
                     print(f"Failed to post data: {response.status}")
 
     except Exception as error:
-        exception_type, exception_object, exception_traceback = sys.exc_info()
+        _, _, exception_traceback = sys.exc_info()
         filename = exception_traceback.tb_frame.f_code.co_filename
         line_no = exception_traceback.tb_lineno
-        raise Exception(f"{filename}の{line_no}行目でエラーが発生しました。詳細：{error}")
+        raise Exception(
+            f"{filename}の{line_no}行目でエラーが発生しました。詳細：{error}"
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main(cookie_path=sys.argv[1]))
--- src/twi_gui.py
+++ src/twi_gui.py
@@ -19,49 +19,59 @@ async def main(cookie_path, twi_num):
             client.load_cookies(cookie_path)
         else:
             raise FileNotFoundError(
-                errno.ENOENT, os.strerror(errno.ENOENT), cookie_path)
+                errno.ENOENT, os.strerror(errno.ENOENT), cookie_path
+            )
 
         bookmarks = await client.get_bookmarks(count=twi_num)
 
         items = []
         for bookmark in bookmarks:
-            if not hasattr(bookmark, 'media') or not bookmark.media:
+            if not hasattr(bookmark, "media") or not bookmark.media:
                 if bookmark.thumbnail_url:
-                    items.append({
-                        "url": bookmark.thumbnail_url,
-                        "name": f"@{bookmark.user.screen_name} {bookmark.text}",
-                        "website": f"https://x.com/{bookmark.user.screen_name}/status/{bookmark.id}/",
-                        "tags": [],
-                        "modificationTime": int(datetime.now(timezone.utc).timestamp()*1000),
-                        "headers": {
-                            "referer": "x.com"
+                    items.append(
+                        {
+                            "url": bookmark.thumbnail_url,
+                            "name": f"@{bookmark.user.screen_name} {bookmark.text}",
+                            "website": f"https://x.com/{bookmark.user.screen_name}/status/{bookmark.id}/",
+                            "tags": [],
+                            "modificationTime": int(
+                                datetime.now(timezone.utc).timestamp() * 1000
+                            ),
+                            "headers": {"referer": "x.com"},
                         }
-                    })
-                if hasattr(bookmark, 'quote') and bookmark.quote is not None:
-                    if hasattr(bookmark.quote, 'media') and bookmark.quote.media is not None:
-                        items.append({
-                            "url": bookmark.quote.media[0].get('media_url_https'),
+                    )
+                if (
+                    hasattr(bookmark, "quote")
+                    and bookmark.quote is not None
+                    and hasattr(bookmark.quote, "media")
+                    and bookmark.quote.media is not None
+                ):
+                    items.append(
+                        {
+                            "url": bookmark.quote.media[0].get("media_url_https"),
                             "name": f"@{bookmark.quote.user.screen_name} {bookmark.quote.text}",
                             "website": f"https://x.com/{bookmark.quote.user.screen_name}/status/{bookmark.quote.id}/",
                             "tags": [],
-                            "modificationTime": int(datetime.now(timezone.utc).timestamp()*1000),
-                            "headers": {
-                                "referer": "x.com"
-                            }
-                        })
-                    continue
+                            "modificationTime": int(
+                                datetime.now(timezone.utc).timestamp() * 1000
+                            ),
+                            "headers": {"referer": "x.com"},
+                        }
+                    )
                 continue
-            for i, media in enumerate(bookmark.media):
-                items.append({
-                    "url": media.get('media_url_https'),
-                    "name": f"@{bookmark.user.screen_name} {bookmark.text}",
-                    "website": f"https://x.com/{bookmark.user.screen_name}/status/{bookmark.id}/",
-                    "tags": [],
-                    "modificationTime": int(datetime.now(timezone.utc).timestamp()*1000),
-                    "headers": {
-                        "referer": "x.com"
+            for media in bookmark.media:
+                items.append(
+                    {
+                        "url": media.get("media_url_https"),
+                        "name": f"@{bookmark.user.screen_name} {bookmark.text}",
+                        "website": f"https://x.com/{bookmark.user.screen_name}/status/{bookmark.id}/",
+                        "tags": [],
+                        "modificationTime": int(
+                            datetime.now(timezone.utc).timestamp() * 1000
+                        ),
+                        "headers": {"referer": "x.com"},
                     }
-                })
+                )
 
         data = {
             "items": items,
@@ -69,20 +79,27 @@ async def main(cookie_path, twi_num):
 
         # Send the POST request
         async with aiohttp.ClientSession() as session:
-            async with session.post(f"http://127.0.0.1:41595/api/item/addFromURLs", json=data) as response:
+            async with session.post(
+                "http://127.0.0.1:41595/api/item/addFromURLs", json=data
+            ) as response:
                 if response.status == 200:
                     result = await response.json()
                     print(result)
                     for bookmark in bookmarks:
-                        if not hasattr(bookmark, 'media') or not bookmark.media:
+                        if not hasattr(bookmark, "media") or not bookmark.media:
                             if bookmark.thumbnail_url:
                                 await bookmark.delete_bookmark()
-                            if hasattr(bookmark, 'quote') and bookmark.quote is not None:
-                                if hasattr(bookmark.quote, 'media') and bookmark.quote.media is not None:
+                            if (
+                                hasattr(bookmark, "quote")
+                                and bookmark.quote is not None
+                            ):
+                                if (
+                                    hasattr(bookmark.quote, "media")
+                                    and bookmark.quote.media is not None
+                                ):
                                     await bookmark.delete_bookmark()
                             continue
                         await bookmark.delete_bookmark()
-
                 else:
                     print(f"Failed to post data: {response.status}")
 
@@ -90,7 +107,9 @@ async def main(cookie_path, twi_num):
         exception_type, exception_object, exception_traceback = sys.exc_info()
         filename = exception_traceback.tb_frame.f_code.co_filename
         line_no = exception_traceback.tb_lineno
-        raise Exception(f"{filename}の{line_no}行目でエラーが発生しました。詳細：{error}")
+        raise Exception(
+            f"{filename}の{line_no}行目でエラーが発生しました。詳細：{error}"
+        )
 
 
 def run_main():
@@ -115,15 +134,17 @@ cookie_path_label.pack()
 cookie_path_entry = tk.Entry(root, textvariable=cookie_path_var, width=50)
 cookie_path_entry.pack()
 cookie_path_button = tk.Button(
-    root, text="Cookieファイルを選ぶ", command=select_cookie_file)
+    root, text="Cookieファイルを選ぶ", command=select_cookie_file
+)
 cookie_path_button.pack()
 
 # Tweet number slider
 twi_num_var = tk.IntVar(value=twi_num)
 twi_num_label = tk.Label(root, text="チェックするBookmarkの数(レート制限注意):")
 twi_num_label.pack()
-twi_num_slider = tk.Scale(root, from_=1, to=100,
-                          orient=tk.HORIZONTAL, variable=twi_num_var)
+twi_num_slider = tk.Scale(
+    root, from_=1, to=100, orient=tk.HORIZONTAL, variable=twi_num_var
+)
 twi_num_slider.pack()
 
 # Run button
